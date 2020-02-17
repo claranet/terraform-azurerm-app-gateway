995a5c0aa23c250fc07dd9e4d455e535aad6a0a9
Rémi Beaufils <remi.beaufils@fr.clara.net>
2020-02-17 11:50:10 +0100

    AZ-183: Add name_prefix variable to default naming for resources

--- locals.tf
+++ locals.tf
@@ -1,5 +1,6 @@
 locals {
-  default_name = "${var.stack}-${var.client_name}-${var.location_short}-${var.environment}"
+  name_prefix  = var.name_prefix != "" ? replace(var.name_prefix, "/[a-z0-9]$/", "$0-") : ""
+  default_name = "${local.name_prefix}${var.stack}-${var.client_name}-${var.location_short}-${var.environment}"
 
   appgw_name = var.appgw_name != "" ? var.appgw_name : join("-", [local.default_name, "appgw"])
 
