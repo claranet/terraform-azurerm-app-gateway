d4239190ef9833bc4de1b2fc742fe07bff7460a7
Rémi Beaufils <remi.beaufils@fr.clara.net>
2020-05-14 15:17:22 +0200

    AZ-183: update outputs

--- outputs.tf
+++ outputs.tf
@@ -45,7 +45,7 @@ output "appgw_backend_http_settings_ids" {
 
 output "appgw_backend_http_settings_probe_ids" {
   description = "List of probe Ids from backend HTTP settings."
-  value       = zipmap(azurerm_application_gateway.app_gateway.backend_http_settings.*.id, azurerm_application_gateway.app_gateway.backend_http_settings.*.probe_id)
+  value       = azurerm_application_gateway.app_gateway.backend_http_settings.*.probe_id
 }
 
 output "appgw_frontend_ip_configuration_ids" {
@@ -85,32 +85,32 @@ output "appgw_request_routing_rule_ids" {
 
 output "appgw_request_routing_rule_http_listener_ids" {
   description = "List of HTTP listener Ids attached to request routing rules."
-  value       = zipmap(azurerm_application_gateway.app_gateway.request_routing_rule.*.id, azurerm_application_gateway.app_gateway.request_routing_rule.*.http_listener_id)
+  value       = azurerm_application_gateway.app_gateway.request_routing_rule.*.http_listener_id
 }
 
 output "appgw_request_routing_rule_backend_address_pool_ids" {
   description = "List of backend address pool Ids attached to request routing rules."
-  value       = zipmap(azurerm_application_gateway.app_gateway.request_routing_rule.*.id, azurerm_application_gateway.app_gateway.request_routing_rule.*.backend_address_pool_id)
+  value       = azurerm_application_gateway.app_gateway.request_routing_rule.*.backend_address_pool_id
 }
 
 output "appgw_request_routing_rule_backend_http_settings_ids" {
   description = "List of HTTP settings Ids attached to request routing rules."
-  value       = zipmap(azurerm_application_gateway.app_gateway.request_routing_rule.*.id, azurerm_application_gateway.app_gateway.request_routing_rule.*.backend_http_settings_id)
+  value       = azurerm_application_gateway.app_gateway.request_routing_rule.*.backend_http_settings_id
 }
 
 output "appgw_request_routing_rule_redirect_configuration_ids" {
   description = "List of redirect configuration Ids attached to request routing rules."
-  value       = zipmap(azurerm_application_gateway.app_gateway.request_routing_rule.*.id, azurerm_application_gateway.app_gateway.request_routing_rule.*.redirect_configuration_id)
+  value       = azurerm_application_gateway.app_gateway.request_routing_rule.*.redirect_configuration_id
 }
 
 output "appgw_request_routing_rule_rewrite_rule_set_ids" {
   description = "List of rewrite rule set Ids attached to request routing rules."
-  value       = zipmap(azurerm_application_gateway.app_gateway.request_routing_rule.*.id, azurerm_application_gateway.app_gateway.request_routing_rule.*.rewrite_rule_set_id)
+  value       = azurerm_application_gateway.app_gateway.request_routing_rule.*.rewrite_rule_set_id
 }
 
 output "appgw_request_routing_rule_url_path_map_ids" {
   description = "List of URL path map Ids attached to request routing rules."
-  value       = zipmap(azurerm_application_gateway.app_gateway.request_routing_rule.*.id, azurerm_application_gateway.app_gateway.request_routing_rule.*.url_path_map_id)
+  value       = azurerm_application_gateway.app_gateway.request_routing_rule.*.url_path_map_id
 }
 
 output "appgw_ssl_certificate_ids" {
@@ -125,17 +125,17 @@ output "appgw_url_path_map_ids" {
 
 output "appgw_url_path_map_default_backend_address_pool_ids" {
   description = "List of default backend address pool Ids attached to URL path maps."
-  value       = zipmap(azurerm_application_gateway.app_gateway.url_path_map.*.id, azurerm_application_gateway.app_gateway.url_path_map.*.default_backend_address_pool_id)
+  value       = azurerm_application_gateway.app_gateway.url_path_map.*.default_backend_address_pool_id
 }
 
 output "appgw_url_path_map_default_backend_http_settings_ids" {
   description = "List of default backend HTTP settings Ids attached to URL path maps."
-  value       = zipmap(azurerm_application_gateway.app_gateway.url_path_map.*.id, azurerm_application_gateway.app_gateway.url_path_map.*.default_backend_http_settings_id)
+  value       = azurerm_application_gateway.app_gateway.url_path_map.*.default_backend_http_settings_id
 }
 
 output "appgw_url_path_map_default_redirect_configuration_ids" {
   description = "List of default redirect configuration Ids attached to URL path maps."
-  value       = zipmap(azurerm_application_gateway.app_gateway.url_path_map.*.id, azurerm_application_gateway.app_gateway.url_path_map.*.default_redirect_configuration_id)
+  value       = azurerm_application_gateway.app_gateway.url_path_map.*.default_redirect_configuration_id
 }
 
 output "appgw_custom_error_configuration_ids" {
